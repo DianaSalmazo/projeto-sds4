bab6a87c0937b2437f05f31bb1166683c2e66612
DianaSalmazo <dcrsalmazo@gmail.com>
2021-09-06 21:38:55 -0300

    Basic static components

---
+++ frontend/src/components/DataTable/index.tsx
@@ -0,0 +1,94 @@
+function DataTable() {
+    return (
+        <div className="table-responsive">
+            <table className="table table-striped table-sm">
+                <thead>
+                    <tr>
+                        <th>Data</th>
+                        <th>Vendedora</th>
+                        <th>Clientes visitados</th>
+                        <th>Negócios fechados</th>
+                        <th>Valor</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Diana Prince</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Jean Grey</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Natalia Romanoff</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Wanda Maximoff</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Ororo Munroe</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Jessica Jones</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Harleen Frances Quinzel</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Barbara Gordon</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr><tr>
+                        <td>22/04/2021</td>
+                        <td>Pamela Isley</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr><tr>
+                        <td>22/04/2021</td>
+                        <td>Susan Richards</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default DataTable;
+
+
+
+
